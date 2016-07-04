52490b45adcd9c0e7d7a7b60fe9646079c40b0f1
ExC0d3 <sharma.abhinav503@gmail.com>
2016-07-04 22:29:42 +0530

    Adding login screen component

--- server/Components/app.component.ts
+++ server/Components/app.component.ts
@@ -1,10 +1,11 @@
 import {Component} from '@angular/core';
 import {FormComponent} from './form.component';
+import {LoginComponent} from './login.component';
 
 @Component({
 	selector:'app',
 	templateUrl:'Components/Templates/app.html',
-	directives:[FormComponent]
+	directives:[LoginComponent, FormComponent]
 })
 
 export class AppComponent {
---
+++ server/Components/login.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+	templateUrl:'Components/Templates/login.html',
+	selector:'login'
+})
+
+export class LoginComponent{
+	
+}
